e76904bb7c12a082958fe33d74c961537a2c1460
Abdullah Aljahdali <Alja7dali@gmail.com>
2021-08-29 04:11:01 +0300

    👋 hello, swep-kitura!

--- Sources/SwepKituraSupport/SwepKituraSupport.swift
+++ Sources/SwepKituraSupport/SwepKituraSupport.swift
@@ -3,12 +3,12 @@ import Kitura
 
 extension RouterResponse {
   public func send(_ node: Document) {
-    self.headers.setType("html")
+    self.headers.setType("html", charset: "utf-8")
     self.send(node.render())
   }
 
   public func send(_ node: Stylesheet) {
-    self.headers.setType("css")
+    self.headers.setType("css", charset: "utf-8")
     self.send(node.render())
   }
 }
